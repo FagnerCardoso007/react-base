3f2fabc82fb5a3948121dbae36ce993172167401
Fagner-Cardoso <cardosoluzinete@terra.com.br>
2022-03-21 18:23:35 -0300

    confirmar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import * as Style from './Header.style';
 
-const Header = () => (
-  <Style.Wrapper>
+const Header = () => {
+ return (
+ <Style.Wrapper>
     <h1>Header</h1>
   </Style.Wrapper>
 );
+ }
 
 export default Header;
